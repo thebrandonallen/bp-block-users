c322ad210a91316fd8476caf87b7cce03ea0ca09
Brandon Allen <cnorris23@gmail.com>
2017-02-16 23:02:01 -0600

    Add incomplete tests for `BPBU_User`

---
+++ tests/phpunit/testcases/user.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Tests for BPBU_User.
+ *
+ * @package BP_Block_Users
+ * @subpackage Tests
+ */
+
+/**
+ * The BPBU_Tests_User test class.
+ *
+ * @since 0.2.0
+ *
+ * @group functions
+ * @group users
+ */
+class BPBU_Tests_User extends BP_UnitTestCase {
+
+	/**
+	 * Test BPBU_User::block.
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers BPBU_User::block
+	 */
+	public function test_block() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test BPBU_User::unblock.
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers BPBU_User::unblock
+	 */
+	public function test_unblock() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test BPBU_User::update_expiration.
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers BPBU_User::update_expiration
+	 */
+	public function test_update_expiration() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test BPBU_User::get_expiration.
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers BPBU_User::get_expiration
+	 */
+	public function test_get_expiration() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test BPBU_User::is_blocked.
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers BPBU_User::is_blocked
+	 */
+	public function test_is_blocked() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test BPBU_User::get_blocked_user_ids.
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers BPBU_User::get_blocked_user_ids
+	 */
+	public function test_get_blocked_user_ids() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test BPBU_User::get_blocked_users.
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers BPBU_User::get_blocked_users
+	 */
+	public function test_get_blocked_users() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test BPBU_User::destroy_sessions.
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers BPBU_User::destroy_sessions
+	 */
+	public function test_destroy_sessions() {
+		$this->markTestIncomplete();
+	}
+}
