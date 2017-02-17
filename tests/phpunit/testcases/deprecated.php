638e1bf66d29f5b03a8ddf8181be236e4b91cb0b
Brandon Allen <cnorris23@gmail.com>
2017-02-17 15:23:02 -0600

    Refactor tests to account for all the deprecation
    * Consolidate deprecated functions into a single file

---
+++ tests/phpunit/testcases/deprecated.php
@@ -0,0 +1,254 @@
+<?php
+/**
+ * BP Block Users tests for deprecated functions.
+ *
+ * @since 0.2.0
+ *
+ * @group functions
+ * @group deprecated
+ *
+ * @package BP_Block_Users
+ * @subpackage Tests
+ */
+
+/**
+ * BP Block Users tests for deprecated functions.
+ */
+class BPBU_Tests_Deprecated extends BP_UnitTestCase {
+
+	/* Admin ******************************************************************/
+
+	/**
+	 * Test for `tba_bp_block_users_settings_fields()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_block_users_settings_fields
+	 */
+	function test_tba_bp_block_users_settings_fields() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test for `tba_bp_block_users_update_user_settings()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_block_users_update_user_settings
+	 */
+	function test_tba_bp_block_users_update_user_settings() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test for `tba_bp_block_users_row_actions()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_block_users_row_actions
+	 */
+	function test_tba_bp_block_users_row_actions() {
+		$this->markTestIncomplete();
+	}
+
+	/* User Functions *********************************************************/
+
+	/**
+	 * Test for `tba_bp_block_user()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_block_user
+	 */
+	function test_tba_bp_block_user() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test for `tba_bp_unblock_user()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_unblock_user
+	 */
+	function test_tba_bp_unblock_user() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test for `tba_bp_update_blocked_user_expiration()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_update_blocked_user_expiration
+	 */
+	function test_tba_bp_update_blocked_user_expiration() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test for `tba_bp_get_blocked_user_expiration()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_get_blocked_user_expiration
+	 */
+	function test_tba_bp_get_blocked_user_expiration() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test for `tba_bp_is_user_blocked()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_is_user_blocked
+	 */
+	function test_tba_bp_is_user_blocked() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test for `tba_bp_get_blocked_user_ids()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_get_blocked_user_ids
+	 */
+	function test_tba_bp_get_blocked_user_ids() {
+		$this->markTestIncomplete();
+	}
+
+	/* Notification Emails ****************************************************/
+
+	/**
+	 * Test for `tba_bp_block_users_block_notifications()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_block_users_block_notifications
+	 */
+	function test_tba_bp_block_users_block_notifications() {
+		$this->markTestIncomplete();
+	}
+
+	/* Authentication *********************************************************/
+
+	/**
+	 * Test for `tba_bp_prevent_blocked_user_login()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_prevent_blocked_user_login
+	 */
+	function test_tba_bp_prevent_blocked_user_login() {
+		$this->markTestIncomplete();
+	}
+
+	/* Sub-nav/Admin Bar Menus ************************************************/
+
+	/**
+	 * Test for `tba_bp_block_user_settings_sub_nav()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_block_user_settings_sub_nav
+	 */
+	function test_tba_bp_block_user_settings_sub_nav() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test for `tba_bp_block_users_admin_bar_admin_menu()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_block_users_admin_bar_admin_menu
+	 */
+	function test_tba_bp_block_users_admin_bar_admin_menu() {
+		$this->markTestIncomplete();
+	}
+
+	/* Settings Actions *******************************************************/
+
+	/**
+	 * Test for `tba_bp_settings_action_block_user()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_settings_action_block_user
+	 */
+	function test_tba_bp_settings_action_block_user() {
+		$this->markTestIncomplete();
+	}
+
+	/* Template ***************************************************************/
+
+	/**
+	 * Test for `tba_bp_block_user_settings_message()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_block_user_settings_message
+	 */
+	function test_tba_bp_block_user_settings_message() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test for `tba_bp_get_block_user_settings_message()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_get_block_user_settings_message
+	 */
+	function test_tba_bp_get_block_user_settings_message() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test for `tba_bp_block_users_show_settings_message()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_block_users_show_settings_message
+	 */
+	function test_tba_bp_block_users_show_settings_message() {
+		$this->markTestIncomplete();
+	}
+
+	/* Theme Compat ***********************************************************/
+
+	/**
+	 * Test for `tba_bp_block_user_settings_load_template_filter()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_block_user_settings_load_template_filter
+	 */
+	function test_tba_bp_block_user_settings_load_template_filter() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test for `tba_bp_block_user_get_template_directory()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_block_user_get_template_directory
+	 */
+	function test_tba_bp_block_user_get_template_directory() {
+		$this->markTestIncomplete();
+	}
+
+	/**
+	 * Test for `tba_bp_settings_screen_block_user()`
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers ::tba_bp_settings_screen_block_user
+	 */
+	function test_tba_bp_settings_screen_block_user() {
+		$this->markTestIncomplete();
+	}
+}
