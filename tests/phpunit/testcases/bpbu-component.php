2e1cff71a14da233b2b6bb4eec94f29358131da4
Brandon Allen <cnorris23@gmail.com>
2017-04-10 01:16:16 -0500

    Tests: Add test for `BPBU_Component:: filter_deprecated_meta_keys()`

--- tests/phpunit/testcases/bpbu-component.php
+++ tests/phpunit/testcases/bpbu-component.php
@@ -48,4 +48,28 @@ class BPBU_Tests_BPBU_Component extends BP_UnitTestCase {
 		$this->assertWPError( $user );
 		$this->assertContains( $expected_message, $actual_message );
 	}
+
+	/**
+	 * Test `BPBU_Component::filter_deprecated_meta_keys()`.
+	 *
+	 * @since 0.2.0
+	 *
+	 * @covers BPBU_Component::filter_deprecated_meta_keys
+	 * @expectedIncorrectUsage tba_bp_user_blocked
+	 * @expectedIncorrectUsage tba_bp_user_blocked_expiration
+	 */
+	public function test_filter_deprecated_meta_keys() {
+
+		$u = $this->factory->user->create();
+
+		$bpbu_component = buddypress()->block_users;
+
+		$this->assertNull( $bpbu_component->filter_deprecated_meta_keys( null, $u, 'test_meta_key', true ) );
+
+		BPBU_User::block( $u );
+		$blocked_meta    = $bpbu_component->filter_deprecated_meta_keys( null, $u, 'tba_bp_user_blocked', true );
+		$expiration_meta = $bpbu_component->filter_deprecated_meta_keys( null, $u, 'tba_bp_user_blocked_expiration', true );
+		$this->assertEquals( '1', $blocked_meta );
+		$this->assertEquals( '3000-01-01 00:00:00', $expiration_meta );
+	}
 }
